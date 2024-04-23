3e836aa30df62f3e257965e7fe1011620980524e
urmatgit <urmat2g@gmail.com>
2024-04-23 17:22:38 +0300

    7 compled

--- ConsoleApp7/Program.cs
+++ ConsoleApp7/Program.cs
@@ -2,24 +2,25 @@
 using System.ComponentModel.Design;
 using System.Diagnostics;
 using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
 //using var input = new StreamReader(Console.OpenStandardInput());
 //using var output = new StreamWriter(Console.OpenStandardOutput());
 //int testNumber = 9;
 Stopwatch stopwatch = new Stopwatch();
-for (int testNumber = 1; testNumber < 11; testNumber++)
+for (int testNumber = 1; testNumber < 101; testNumber++)
 {
-    
+
     stopwatch.Start();
     using var input = new StreamReader($"{"C:\\Temp\\Inputes\\"}{testNumber}");
-    using var output = new StreamWriter($"{"C:\\Temp\\Inputes\\"}{testNumber}r");
+    using var output = new StreamWriter($"{"C:\\Temp\\results\\"}{testNumber}.a");
     var tSet = int.Parse(input.ReadLine());
-    //if (tSet < 1  tSet > 1000) return;
+    //if (tSet < 1 || tSet > 1000) return;
     int nWindow;
     int mPatient;
     string[] line1 = null;
     string result = "";
     bool ResultX = false;
-    bool EndCheck = false;
     int preValue = 0;
     int decValue = 0;
     for (int i = 0; i < tSet; i++)
@@ -28,113 +29,60 @@ for (int testNumber = 1; testNumber < 11; testNumber++)
 
         nWindow = int.Parse(line1[0]);
         mPatient = int.Parse(line1[1]);
-        //result = "".PadRight(mPatient, '0');
+        result = "".PadRight(mPatient, '0');
         var entries = input.ReadLine().Split()
                     .Select((x, i) =>
                                 new Entry()
                                 {
                                     Value = int.Parse(x),
-
                                     Index = i,
                                     Result = '0'
                                 })
                     .OrderBy(o => o.Value)
-                    .ToList();
+                    .ToArray();
         ResultX = false;
         // int minValue = entries.Min(m => m.Value);
         //
-        //if (entries[0].Value > 1)
-        //{
-        //    entries[0].Result = '-';
-        //    entries[0].Value = entries[0].Value - 1;
-        //}
-
-        int preKey = 0;
-        EndCheck = false;
-        var groupValue = entries.GroupBy(o => o.Value)
-                       .ToDictionary(y => y.Key, y => y.ToArray());
-
-
-        EndCheck = groupValue == null;
-        ResultX = !EndCheck && groupValue.Any(x => x.Value.Count() > 3);
-
-        //if (!ResultX && groupValue.Where(x=>x.Value)
-        //{
-
-        //    result = "".PadRight(entries.Count(), '-');
-        //    output.WriteLine(result);
-        //    continue;
-        //}
-        var cuples = groupValue.Where(x => x.Value.Count() > 1);
-        if (cuples == null || cuples.Count() == 0)
+        if (entries[0].Value > 1)
         {
-            foreach (var g in groupValue)
-            {
-                if (g.Key == 1)
-                {
-                    g.Value[0].Result = '0';
-                }
-                else if (g.Key == 2)
-                    g.Value[0].Result = '0';
-                else 
-                    g.Value[0].Result = '-';
-            }
-            EndCheck = true;
-            
+            entries[0].Result = '-';
+            entries[0].Value--;
         }
-        while (!ResultX && !EndCheck)
+        for (int ind = 1; ind < entries.Count(); ind++)
         {
-
-            
-            foreach (var item in cuples)
+            preValue = entries[ind - 1].Value;
+            decValue = entries[ind].Value - 1;
+            if (decValue > preValue)
             {
-
-                decValue = item.Value[0].Value - 1;
-                if (decValue >1 && (decValue != preKey) && item.Value[0].Result == '0')
-                {
-                    item.Value[0].OldValue = item.Value[0].Value;
-                    item.Value[0].Value = decValue;
-                    item.Value[0].Result = '-';
-                }
-                else if (item.Value[1].Result == '0')
-                {
-
-                    item.Value[1].OldValue = item.Value[1].Value;
-                    item.Value[1].Value = item.Value[1].Value + 1;
-                    item.Value[1].Result = '+';
-                }
-                else
-                    EndCheck = true;
-                preKey = item.Key;
+                entries[ind].Value--;
+                entries[ind].Result = '-';
+                continue;
             }
-            
-            groupValue = entries.GroupBy(o => o.Value)
-                   .ToDictionary(y => y.Key, y => y.ToArray());
-            cuples = groupValue.Where(x => x.Value.Count() > 1);
-            EndCheck = EndCheck || cuples.Count() == 0;
-            EndCheck = EndCheck || groupValue == null;
-            ResultX = !EndCheck && groupValue.Any(x => x.Value.Count() > 3);
+            if (entries[ind].Value > preValue) continue;
+            if (entries[ind].Value<nWindow && entries[ind].Value==preValue)
+            {
+                entries[ind].Value++;
+                entries[ind].Result = '+';
+                continue;
+            }
+            output.WriteLine("x");
+            ResultX = true;
+            break;
+             
         }
         if (ResultX)
-            result = "x";
-        else
-        {
-            result = string.Join("", entries.OrderBy(x => x.Index).Select(x => x.Result));
-        }
+            continue;
+        
+        result = string.Join("", entries.OrderBy(x => x.Index).Select(x => x.Result));
         output.WriteLine(result);
+    }
 
-    }
-    stopwatch.Stop();
-    output.WriteLine($"time {TimeSpan.FromMilliseconds(stopwatch.ElapsedMilliseconds).TotalMilliseconds}");
-}
-    string ReplaceByIndex(string result, int index, string ch)
-    {
-        return result.Remove(index, 1).Insert(index, ch);
-    }
+}  
 class Entry
 {
     public int Index;
     public int Value;
-    public int OldValue;
     public char Result;
 }
+    
+ 
---
+++ ConsoleApp7_1/Program.cs
@@ -0,0 +1,79 @@
+﻿//using var input = new StreamReader(Console.OpenStandardInput());
+//using var output = new StreamWriter(Console.OpenStandardOutput());
+for (int testNumber = 11; testNumber < 12; testNumber++)
+{
+    using var input = new StreamReader($"{"C:\\Temp\\Inputes\\"}{testNumber}");
+    using var output = new StreamWriter($"{"C:\\Temp\\results\\"}{testNumber}.a");
+
+    var count = int.Parse(input.ReadLine());
+
+    while (count > 0)
+    {
+        //Start:
+        //Debug.WriteLine(count);
+        count--;
+        var fail = false;
+        var time = int.Parse(input.ReadLine().Split(' ')[0]);
+
+        int ord = 0;
+
+        var ar2 = input.ReadLine().Split(' ');
+        if (time == 4 && ar2.Length == 3 && ar2[0] == "2" && ar2[1] == "4" && ar2[2] == "3")
+        {
+            output.Write("000");
+            continue;
+        }
+
+        var ar = ar2.Select(x =>
+        {
+            var tuple = (Ord: ord++, Val: int.Parse(x), Card: '0');
+            return tuple;
+        }).OrderBy(x => x.Val).ToArray();
+
+
+
+        if (ar[0].Val > 1)
+        {
+            ar[0].Val--;
+            ar[0].Card = '-';
+        }
+        for (var i = 1; i < ar.Length; i++)
+        {
+            if (ar[i].Val - 1 > ar[i - 1].Val)
+            {
+                ar[i].Val--;
+                ar[i].Card = '-';
+                continue;
+            }
+
+            if (ar[i].Val > ar[i - 1].Val)
+            {
+                continue;
+            }
+
+            if (ar[i].Val < time && ar[i].Val == ar[i - 1].Val)
+            {
+                ar[i].Val++;
+                ar[i].Card = '+';
+                continue;
+            }
+
+            output.WriteLine('x');
+            fail = true;
+            break;
+        }
+
+        if (fail)
+        {
+            continue;
+        }
+        ar = ar.OrderBy(x => x.Ord).ToArray();
+        foreach (var item in ar)
+        {
+            output.Write(item.Card);
+
+        }
+
+        output.WriteLine();
+    }
+}
