6e7faa5a2d8bbc76bee8714d1e30a5699e8327fe
urmatgit <urmat2g@gmail.com>
2024-04-26 12:00:46 +0300

    Update Program.cs

--- ConsoleApp8_1/Program.cs
+++ ConsoleApp8_1/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
-Dictionary<string, int> resultBlue = new Dictionary<string, int>();
-Dictionary<string, int> resultRed = new Dictionary<string, int>();
-for (int testNumber = 1; testNumber < 12; testNumber++)
+using System.Diagnostics;
+
+Dictionary<string, subStr> resultBlue = new Dictionary<string, subStr>();
+
+for (int testNumber =1; testNumber < 76; testNumber++)
 {
     using var input = new StreamReader($"{"C:\\Temp\\Inputes\\"}{testNumber}");
     using var output = new StreamWriter($"{"C:\\Temp\\results\\"}{testNumber}.a");
@@ -12,7 +14,7 @@ for (int testNumber = 1; testNumber < 12; testNumber++)
     for (int i = 0; i < tSet; i++)
     {
         resultBlue.Clear();
-        resultRed.Clear();
+        
         var line1 = input.ReadLine().Split();
         var nAllWords = int.Parse(line1[0]);
         var blueWords = int.Parse(line1[1]);
@@ -32,7 +34,7 @@ for (int testNumber = 1; testNumber < 12; testNumber++)
                 for (int n2 = 0; n2 < word.Length; n2++)
                     for (int n3 = 1; n3 <=word.Length-n2; n3++)
                     {
-                        addStr(word.Substring(n2, n3), 0, resultBlue);
+                        addStr(word.Substring(n2, n3), 0,n, resultBlue);
                         
 
 
@@ -45,7 +47,7 @@ for (int testNumber = 1; testNumber < 12; testNumber++)
                 for (int n2 = 0; n2 < word.Length; n2++)
                     for (int n3 = 1; n3 <= word.Length - n2; n3++)
                     {
-                        addStr(word.Substring(n2, n3), 1, resultBlue);
+                        addStr(word.Substring(n2, n3), 1,n, resultBlue);
                     }
             }
             else  //red
@@ -54,7 +56,7 @@ for (int testNumber = 1; testNumber < 12; testNumber++)
                 for (int n2 = 0; n2 < word.Length; n2++)
                     for (int n3 = 1; n3 <= word.Length - n2; n3++)
                     {
-                        addStr(word.Substring(n2, n3 ), 2, resultBlue);
+                        addStr(word.Substring(n2, n3 ), 2,n, resultBlue);
                     }
             }
             else //white
@@ -63,7 +65,7 @@ for (int testNumber = 1; testNumber < 12; testNumber++)
                 for (int n2 = 0; n2 < word.Length; n2++)
                     for (int n3 = 1; n3 <= word.Length - n2; n3++)
                     {
-                        addStr(word.Substring(n2, n3 ), 3, resultBlue);
+                        addStr(word.Substring(n2, n3 ), 3,n, resultBlue);
                         
 
                     }
@@ -76,32 +78,55 @@ for (int testNumber = 1; testNumber < 12; testNumber++)
             //if (resultRed.Keys.Contains(word))
             //    resultRed.Remove(word);
         }
-        var endResult=resultBlue.Where(x=>x.Value!= -999).OrderByDescending(o=>o.Value).ThenByDescending(o=>o.Key.Length).ThenBy(o=>o.Key).ToList();
+        var endResult=resultBlue.Where(x=>x.Value.count>=0  && (x.Value.type==1 || x.Value.type == 2) ).OrderByDescending(o=>o.Value.count).ThenByDescending(o=>o.Key.Length).ThenBy(o=>o.Key).ToList();
        // var endResultRed = resultRed.Where(x => x.Value != -999).OrderByDescending(o => o.Value).ThenByDescending(o => o.Key.Length).ThenBy(o => o.Key).ToList();
 
-        if (endResult.Count > 0)
-            output.WriteLine($"{endResult[0].Key} {endResult[0].Value}");
+        if (endResult.Count > 0 )
+            output.WriteLine($"{endResult[0].Key} {endResult[0].Value.count}");
         else
             output.WriteLine("tkhapjiabb 0");
 
     }
 }
-void addStr(string subStr,int type, Dictionary<string,int> res)
+void addStr(string subStr,int type, int wordnumber, Dictionary<string, subStr> res)
 {
     if (!res.ContainsKey(subStr))
     {
-        res.Add(subStr, 0);
+        res.Add(subStr, new subStr());
     }
-    switch(type)
+    if (res[subStr].WordNumber == wordnumber)
     {
-        case 1:
-            res[subStr]++;break;
-        case 2:
-            res[subStr]--; break;
+        return;
+    }
+    res[subStr].WordNumber = wordnumber;
+    switch (type)
+    {
         case 3:
+            if (res[subStr].count==0 && res[subStr].type==2)
+                res[subStr].type = 1;
             break;
+        case 1 :
+             
+                res[subStr].count++;
+                res[subStr].type = type;
+     
+             
+            break;
+        case 2:
+            res[subStr].count--;
+            res[subStr].type = type;
+            break;
+        
+            
         default:
-            res[subStr]=-999; break;
+            res[subStr].count=int.MinValue;
+            break;
 
     }
+}
+class subStr
+{
+    public int count { get; set; }
+    public int type { get; set; }
+    public int WordNumber { get; set; }
 }
