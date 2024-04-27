31b6d1d5d509508a3d34992b438d090ad9390c48
urmatgit <urmat2g@gmail.com>
2024-04-27 16:15:22 +0300

    add task 9

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.Text.RegularExpressions;
+
+for (int testNumber = 1; testNumber < 76; testNumber++)
+{
+    using var input = new StreamReader($"{"C:\\Temp\\Inputes\\"}{testNumber}");
+    using var output = new StreamWriter($"{"C:\\Temp\\results\\"}{testNumber}.a");
+
+
+    var tSet = int.Parse(input.ReadLine());
+    Regex reg = new Regex(@"^(M(RCM|CM)*D)+$", RegexOptions.Compiled);
+    for (int i = 0; i < tSet; i++)
+    {
+        int  xyxzyzCount =int.Parse(input.ReadLine());
+        string xyxzyz = input.ReadLine();
+        var result= reg.IsMatch(xyxzyz) ? "YES" : "NO";
+        output.WriteLine(result);
+
+        
+    }
+
+}
